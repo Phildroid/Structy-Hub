ecc58c6ac66e6665fc36ae43dbd46fadfcfaa986
Oyedele Philemon <158113554+Phildroid@users.noreply.github.com>
2025-10-08 09:40:20 +0100

    Solved 'minimum island' (python3)

---
+++ Graph-I/091-minimum-island/minimum-island.py
@@ -0,0 +1,32 @@
+def minimum_island(grid):
+  visited = set()
+  min_size = float('inf')
+
+  for r in range(len(grid)):
+    for c in range(len(grid[0])):
+      current_island = explore_len(grid, r, c, visited)
+      if current_island > 0:
+        min_size = min(min_size, current_size)
+  return min_size
+
+def explore_len(grid, r, c, visited):
+  island_len = 0
+  row_inbounds = 0 <= r < len(grid)
+  col_inbounds = 0 <= c < len(grid[0])
+
+  if not row_inbounds or not col_inbounds:
+    return False
+  if grid[r][c] == 'W':
+    return False
+  pos = (r, c)
+  if pos in visited:
+    return False
+  visited.add(pos)
+  island_len = 1
+
+  island_len += explore_len(grid, r + 1, c, visited)
+  island_len += explore_len(grid, r - 1, c, visited)
+  island_len += explore_len(grid, r, c + 1, visited)
+  island_len += explore_len(grid, r, c - 1, visited)
+  
+  return island_len
